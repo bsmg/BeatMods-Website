77d918e1984f82f697c2072c985cefa7e5ec6c6c
opl- <opl-@users.noreply.github.com>
2019-04-14 18:32:52 +0200

    Footer improvements

--- src/client/app/layouts/default/footer.tsx
+++ src/client/app/layouts/default/footer.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 
 export default class DefaultFooter extends React.Component<{}, {}> {
-  render() {
-    // eslint-disable-next-line
-    return (
-      <React.Fragment>
-        <span>&copy; 2019</span>
-      </React.Fragment>
-    );
-  }
+    render() {
+        // eslint-disable-next-line
+        return (
+            <React.Fragment>
+                <div>
+                    <span>vanZeben and</span>
+                    <a href="https://github.com/beat-saber-modding-group/BeatMods-Website/graphs/contributors">contributors</a>
+                    <span>&copy; 2019</span>
+                </div>
+                <div className="ml-auto">
+                    <a href="https://github.com/beat-saber-modding-group/BeatMods-Website">Contribute on GitHub</a>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
