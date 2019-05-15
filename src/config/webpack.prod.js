a3298fe564ab800fac53ab38d01bd458c3e9173c
Raphaël Thériault <raphael_theriault@outlook.com>
2019-05-15 15:57:19 -0400

    Fixed build scripts

--- src/config/webpack.prod.js
+++ src/config/webpack.prod.js
@@ -1,7 +1,5 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const helpers = require('./helpers');
 const commonConfig = require('./webpack.common');
 module.exports = merge(commonConfig, {
     mode: 'production',
